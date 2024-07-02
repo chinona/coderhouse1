a57a9310a9b613a2f22aa95264d6102f83fa454b
agustin carranza <agustincarranzacarabajal@gmail.com>
2024-07-02 17:44:06 -0300

    mi primer version

---
+++ ejemplo1.py
@@ -0,0 +1,6 @@
+#primero cambiamos la terminal a la de bash
+#lo primero es configurar tu nombre de usuario en la terminal, esto se configura una unica vez en la computadora, no lo tengo q hacer cada vez q arranque un trabajo en git
+#luego hay q abrir la terminal y colocar  git init para crear la carpeta repositorio
+def mensaje():
+    print(" estoy trabajando con git")
+mensaje()
