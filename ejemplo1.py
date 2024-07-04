f07abdefe6abdd4b68b11f17a7cc5e056608ba27
agustin carranza <agustincarranzacarabajal@gmail.com>
2024-07-04 11:35:14 -0300

    ult

--- ejemplo1.py
+++ ejemplo1.py
@@ -18,4 +18,12 @@ multiplicacion(int(input("ingresa el primer numero: ")), input("ingresa el segun
 
 #para ver en q estado están mir archivos ecribir en la terminal git status
 #para agregarlos git add . o git add nombre del archivo
-#git commit para que pase al repositorio
+#git commit -m un mensaje que aclare lo q agregamos a esta version , para que pase al repositorio
+#para citar o ver las versiones guardadas poner git log
+# si quiero verlas reducidas poner git --oneline
+# si quiero volver a una version pasada, debo poner git checkout y el numero o nombre de identificacion de la version, despues podes volver a las versiones actuales con git checkout master (o main si tenes main)
+#si vamos ahcia atras, podemos modificar una version, pero, todo lo q sigue, la linea de tiempo que sigue, se borrará, por tanto, para cambiar versiones pasadas y mezclarlas con las del futuro, devemos poner git merge master y luego pasar a los 3 estados, git add y git commit 
+# "master" es como la rama madre, como la linea de tiempo original
+#para crear otra rama usamos git branch nombre de la rama
+# git branch consulta la cantidad de ramas que tenemos
+# para posicionarme en la nueva rama ongo git checkout nombre de la rama
